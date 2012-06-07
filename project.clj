adaa24929ccd1bd49ea7b2fd662c9e9616a7dc84
Anthony Grimes <i@raynes.me>
2012-06-06 20:05:59 -0500

    Release alpha6.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject gedcom "0.1.0-alpha5"
+(defproject gedcom "0.1.0-alpha6"
   :description "A GEDCOM library written in Clojure."
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [commons-io/commons-io "2.3"]])
