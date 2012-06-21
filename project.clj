38748171357954a8bae9391f7e19f5d97b00c3b6
Anthony Grimes <i@raynes.me>
2012-06-21 18:40:34 -0500

    Add more info to project.clj.

--- project.clj
+++ project.clj
@@ -1,5 +1,9 @@
 (defproject gedcom "0.1.0-alpha8"
   :description "A GEDCOM library written in Clojure."
+  :license {:name "Eclipse Public License - v 1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"
+            :distribution :repo}
+  :url "https://github.com/geni/gedcom"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [commons-io/commons-io "2.3"]
                  [org.clojars.rayne/anselcharset "1.1"]])
