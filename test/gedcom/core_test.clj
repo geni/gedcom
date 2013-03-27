94ad773ee2f195c6c483a7c26bb32e9aac2461dc
Anthony Grimes <i@raynes.me>
2013-03-27 14:30:03 -0700

    Fix the tests.

--- test/gedcom/core_test.clj
+++ test/gedcom/core_test.clj
@@ -6,12 +6,13 @@
 (deftest parser
   (testing "gedcom-line"
     (are [input expected] (= expected (into {} (gedcom-line input)))
-      "0 FOO"           {:level 0 :label nil :tag "FOO" :data nil}
-      "0 @I1@ FOO data" {:level 0 :label "@I1@" :tag "FOO" :data "data"}
-      "0 FOO data"      {:level 0 :label nil :tag "FOO" :data "data"}
-      "0 FOO @I1@"      {:level 0 :label nil :tag "FOO" :data "@I1@"}))
+      "0 FOO"           {:level 0 :label nil :tag "FOO" :suffix nil :data nil}
+      "0 @I1@ FOO data" {:level 0 :label "@I1@" :tag "FOO" :suffix nil :data "data"}
+      "0 FOO data"      {:level 0 :label nil :tag "FOO" :suffix nil :data "data"}
+      "0 FOO @I1@"      {:level 0 :label nil :tag "FOO" :suffix nil :data "@I1@"}
+      "0 @I1@ FOO__EN data" {:level 0 :label "@I1@" :tag "FOO" :suffix "EN" :data "data"}))
 
   (testing "gedcom-line-seq"
     (are [input expected]
-         (= expected (into {} (->> input (map gedcom-line) gedcom-line-seq first))
-      "0 FOO one\n1 CONT two\n1 CONC  three" {:level 0 :label nil :tag "FOO" :data "one\ntwo three"}))))
+         (= expected (into {} (->> input (map gedcom-line) gedcom-line-seq first)))
+         ["0 FOO one" "1 CONT two" "1 CONC  three"] {:level 0 :label nil :tag "FOO" :suffix nil :data "one\ntwo three"})))
